abc2cfffd74f7b2a168713f19969a6e3491fcb4e
geraldojunior03 <geraldojunior.2003@outlook.com>
2025-05-14 10:50:14 -0300

    Adição de seção de experiencias

---
+++ projects/functions/ValidacoesCard.js
@@ -0,0 +1,15 @@
+import dataProjetos from "../../../../projects.json"
+
+const validacaoFront = dataProjetos.automation.length > 6 && (
+  <div className="showMore">
+    <button id="moreButton">Show More</button>
+  </div>
+)
+
+const validacaoBack = dataProjetos.certificates.length > 6 && (
+  <div className="showMore">
+    <button id="moreButton">Show More</button>
+  </div>
+)
+
+export { validacaoFront, validacaoBack }
